fe69c067f7547b7a015b534356098ee840fe20e0
cancancann <73189111+cancancann@users.noreply.github.com>
2021-10-25 19:44:08 +0300

    guncelleme

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log("Merhaba Kodlama.io");
+
+
+let  konutKredileri = ["Konut Kredileri","Emlak Konut Kredileri","Kamu Konut Kredileri","Özel Konut Kredileri"]
+
+console.log("<ul>")
+
+for(let i = 0; i<konutKredileri.length ; i++){
+
+    console.log("<li>"+konutKredileri[i]+"<li/>")
+
+}
+console.log("<ul/>")
